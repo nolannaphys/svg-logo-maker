538fb933c37b8b999b628846e71ebb5d33cd4b0f
Nolan <nolannaphys@gmail.com>
2023-05-25 21:53:59 -0400

    first try at making test files and started index.js

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
+
+let shape;
+function renderShape(selectedShape, fillColor){
+    console.log(selectedShape);
+    if(selectedShape === 'circle'){
+        shape = new Circle();
+        shape.setColor(fillColor);
+        return shape.render();
+    };
+    else if(selectedShape === 'square'){
+        shape = new Square();
+        shape.setColor(fillColor);
+        return shape.render();
+    };
+    else if(selectedShape === 'triangle'){
+        shape = new Triangle();
+        shape.setColor(fillColor);
+        return shape.render();
+    };
+};
+
