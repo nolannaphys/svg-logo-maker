331804c0e9773f9223d92198b740343745fc6bef
Nolan <nolannaphys@gmail.com>
2023-05-25 22:33:14 -0400

    not sure what my issue is. the logo will generate but all variables are undefined

--- index.js
+++ index.js
@@ -9,18 +9,77 @@ function renderShape(selectedShape, fillColor){
     console.log(selectedShape);
     if(selectedShape === 'circle'){
         shape = new Circle();
-        shape.setColor(fillColor);
+        shape.fillColor = fillColor;
         return shape.render();
     };
-    else if(selectedShape === 'square'){
+    if(selectedShape === 'square'){
         shape = new Square();
-        shape.setColor(fillColor);
+        shape.fillColor = fillColor;
         return shape.render();
     };
-    else if(selectedShape === 'triangle'){
+    if(selectedShape === 'triangle'){
         shape = new Triangle();
-        shape.setColor(fillColor);
+        shape.fillColor = fillColor;
         return shape.render();
     };
 };
 
+const generateLogo = ({text, textColor, shape, backgroundColor}) =>
+`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+${renderShape(shape, fillColor)}
+<text x="150" y="125" font-size="60" text-anchor="middle" fill=${this.textColor}>${this.text}</text>
+</svg>`
+
+const questions = [
+    {
+        type: 'list',
+        message: 'Pick a shape for your logo',
+        name: 'shape',
+        choices: [
+            {
+                name: 'circle',
+                value: 'circle'
+            },
+            {
+                name: 'square',
+                value: 'square'
+            },    
+            {
+                name: 'triangle',
+                value: 'triangle'
+            }
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Enter desired color or hexadecimal code for background color',
+        name: 'fillColor'
+    },
+    {
+        type: 'input',
+        message: 'Enter three characters for your logo',
+        name: 'text',
+        maxLength: 3
+    },
+    {
+        type: 'input',
+        message: 'Enter desired color or hexadecimal code for text',
+        name: 'textColor',
+    }
+];
+
+function writeToFile(fileName, data){
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.log(err) : console.log('Generated logo.svg')
+    );
+}
+
+function init(){
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const createLogo = generateLogo(answers)
+        writeToFile('logo.svg', createLogo);
+    })
+}
+init();
